16a9ccd2b8605f986e7a13ffc06884dbce2c37f0
Ky13r13 <kgbock1013@gmail.com>
2026-09-08 15:42:44 -0700

    mkt struct

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
+# include <iostream>
 
+struct MarketData {
+    double price;
+    
+};
 // test push comments
 int main(){
-    
-    return 0 ;
+    MarketData testdata; 
+    testdata.price = 150.30;
+    std::cout<<testdata.price<<"\n";
+
 }
